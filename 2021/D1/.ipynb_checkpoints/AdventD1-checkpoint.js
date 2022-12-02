579694ff1c80133d3c9025dbb20cea2f16bede35
JamesLloydGranify <87337503+JamesLloydGranify@users.noreply.github.com>
2022-12-01 20:10:58 -0700

    Day 1 solution

---
+++ 2021/D1/.ipynb_checkpoints/AdventD1-checkpoint.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const {join} = require('path')
+
+const file = fs.readFileSync(join(__dirname, 'D1A.txt'), 'utf8')
+const inputs = file.split('\n').slice(0, -1).map(n => Number(n))
+// Problem 1
+{
+    let count = 0
+  
+    for (let i = 1; i < inputs.length; i++) {
+      const curr = inputs[i]
+      const prev = inputs[i - 1]
+  
+      if (curr > prev) count++
+    }
+  
+    console.log(`Count 1: ${count}`);
+  }
+  
+  
+  // Problem 2
+  {
+    let count = 0
+  
+    for (let i = 1; i < inputs.length - 2; i++) {
+      const curr = inputs[i] + inputs[i + 1] + inputs[i + 2]
+      const prev = inputs[i - 1] + inputs[i] + inputs[i + 1]
+  
+      if (curr > prev) count++
+    }
+  
+    console.log(`Count 2: ${count}`);
+  }
